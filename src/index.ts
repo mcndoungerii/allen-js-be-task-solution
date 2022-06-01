42d7ee472946bbaabb56ea65dc7d29a3476a8246
Allen <allenmcndounger@gmail.com>
2022-06-01 20:51:40 +0300

    calling a session service in a session controller

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import './externalService';
+import { getSessionService } from './libraries/service';
 
 const app = express();
 const port = 3000;
 
 app.get('/api/sessions/:sessionId', async (req, res) => {
   try {
-    return res.status(501).json();
+    const data = await getSessionService(req.params.sessionId)
+    return res.status(200).json(data);
   } catch (error: any) {
     console.error('Error happened', error);
     return res.status(error.response.status).json({ message: error.response.data });
